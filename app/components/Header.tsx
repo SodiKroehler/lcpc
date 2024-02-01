c2d76885b6fb88a9139fac4b900c5f501c997163
SodiKroehler <sodikroehler@outlook.com>
2024-02-01 14:03:41 -0500

    favicon new image and formatting

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,19 +3,19 @@ import Link from "next/link";
 export default function Header() {
     return (
         <div className={`flex w-full justify-center items-center`}>
-            <Link href="/"  className={`text-light p-1`}>
+            <Link href="/"  className={`text-light p-10 text-lg hover:bg-dark`}>
                 <span className="text-white">Home</span>
             </Link>
-            <Link href="https://www.panebakerfuneralhome.com/plan-ahead/online-pre-planning-form"  className={`text-light p-1`}>
+            <Link href="https://www.panebakerfuneralhome.com/plan-ahead/online-pre-planning-form"  className={`text-light p-10 text-lg hover:bg-dark`}>
                 <span className="text-white">Planning Ahead</span>
             </Link>
-            <Link href="https://www.panebakerfuneralhome.com/about-us/cremation-care-center"  className={`text-light p-1`}>
+            <Link href="https://www.panebakerfuneralhome.com/about-us/cremation-care-center"  className={`text-light p-10 text-lg hover:bg-dark`}>
                 <span className="text-white">Cremation Options</span>
             </Link>
-            <Link href="https://www.panebakerfuneralhome.com/"  className={`text-light p-1`}>
+            <Link href="https://www.panebakerfuneralhome.com/"  className={`text-light p-10 text-lg hover:bg-dark`}>
                 <span className="text-white">Our Funeral Home</span>
             </Link>
-            <Link href="https://www.loyalcompanionpetcremation.com/"  className={`text-light p-1`}>
+            <Link href="https://www.loyalcompanionpetcremation.com/"  className={`text-light p-10 text-lg hover:bg-dark`}>
                 <span className="text-white">Our Pet Crematory</span>
             </Link>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,26 @@ import Header from "./components/Header"
 export default function Home() {
   return (
     <main className="h-full w-full flex-col items-center justify-between bg-white">
-      <h1 className={`text-fg`}>Coming Soon!</h1>
-      {/* <div className={`absolute top-0 py-5 mt-20 w-full bg-fg`}>
+      <div className={`absolute top-0 mt-40 w-full bg-fg`}>
         <Header />
       </div>  
-      <Image src='/img.png' alt='Picture of the funeral home' width={1000} height={1000} className={`h-lvh w-full object-cover`}></Image> */}
+      <Image src='/img.jpg' alt='Picture of the funeral home' width={1000} height={1000} className={`h-lvh w-full object-cover`}></Image>
+      <div className={`absolute flex-row text-center items-center justify-between bottom-0 w-full p-5 bg-fg`}>
+        <div className={`text-center p-5 inline-block`}>
+          <p>Panebaker Funeral Home & Cremation Care Center, Inc. </p>
+          <p>311 Broadway</p>
+          <p>Hanover, PA  17331</p>
+          <p>(717) 637-5194 - Phone</p>
+          <p>www.PanebakerFuneralHome.com</p>
+        </div>  
+        <div className={`text-center p-5 inline-block`}>
+          <p>Loyal Companion Pet Cremation, Inc. </p>
+          <p>43 Amy Way</p>
+          <p>Hanover, PA 17331</p>
+          <p>(717) 698-1970 - Phone</p>
+          <p>www.LoyalCompanionPetCremation.com</p>
+        </div>  
+      </div>  
     </main>
   );
 }
